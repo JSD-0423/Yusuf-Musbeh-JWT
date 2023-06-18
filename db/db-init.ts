321d0e976d0b6752b1557e750c4b5aacf5f051cd
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-06-18 13:45:22 +0300

    fix: connect associations to middle table ( rented_books)

--- db/db-init.ts
+++ db/db-init.ts
@@ -1,8 +1,10 @@
 import { User } from "./models/user";
 import { Book } from "./models/book";
 import { sequelize } from "./connection";
+import { RentedBook } from "./models/rented-books";
 
 export async function dbInit() {
-  Book.belongsToMany(User, { through: "rented_books", timestamps: false });
-  await sequelize.sync();
+  Book.belongsToMany(User, { through: RentedBook, timestamps: false });
+  User.belongsToMany(Book, { through: RentedBook, timestamps: false });
+  await sequelize.sync({ alter: true });
 }
