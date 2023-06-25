7118c8ff568e5a70ecc6a93bc2d12a3d7ae2fd95
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-06-25 19:21:47 +0300

    rafactor: merge public and protect route into one and remove it from app.js

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,7 @@ import { authenticateConnection } from "./db/connection";
 import { dbInit } from "./db/db-init";
 import { configurePassport } from "./utils/configure-passport";
 import passport from "passport";
-import {
-  publicRouter as publicUserRouter,
-  protectedRouter as protectedUserRouter,
-} from "./routes/user-route";
+import { publicRouter as publicUserRouter } from "./routes/user-route";
 import { User } from "./db/models/user";
 
 const app: Express = express();
@@ -14,13 +11,6 @@ app.use(express.json());
 app.use(passport.initialize());
 configurePassport();
 app.use(publicUserRouter);
-// assign passport authenticate middleware for protected routes
-app.use(passport.authenticate("jwt", { session: false }));
-app.use(protectedUserRouter);
-app.get("/", (request, response, next) => {
-  const user: User = request.user;
-  response.send("you can see thi s page");
-});
 authenticateConnection().then(() => {
   dbInit().then(() => {
     app.listen(3000, "localhost", () =>
