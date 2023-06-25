47bb6b345f50d74664391ce068202033d05edb08
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-06-25 19:18:48 +0300

    fix: fix the associations relationships

--- src/db/db-init.ts
+++ src/db/db-init.ts
@@ -1,15 +1,24 @@
 import { User } from "./models/user";
 import { Book } from "./models/book";
 import { sequelize } from "./connection";
-import { RentedBook } from "./models/rented-books";
 
 export async function dbInit() {
-  User.hasMany(RentedBook);
-  RentedBook.belongsTo(User);
-
-  Book.hasMany(RentedBook);
-  RentedBook.belongsTo(Book);
-  Book.belongsToMany(User, { through: RentedBook, timestamps: false });
-
+  // User.hasMany(RentedBook, { sourceKey: "id" });
+  // RentedBook.belongsTo(User, { targetKey: "id" });
+  // Book.hasMany(RentedBook, { sourceKey: "id" });
+  // RentedBook.belongsTo(Book, { targetKey: "id" });
+  // Book.belongsToMany(User, { through: RentedBook, timestamps: false });
+  Book.belongsToMany(User, {
+    through: "rented_books",
+    foreignKeyConstraint: true,
+    foreignKey: "book_id",
+    timestamps: false,
+  });
+  User.belongsToMany(Book, {
+    through: "rented_books",
+    foreignKeyConstraint: true,
+    foreignKey: "user_id",
+    timestamps: false,
+  });
   await sequelize.sync({ alter: true });
 }
